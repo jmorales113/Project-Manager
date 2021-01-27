4c4e23a8bb4052f35702db6a6862c68bbf9ee87c
jmorales113 <jmorales113@gmail.com>
2021-01-26 19:44:14 -0800

    3rd attempt to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require("path")
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: "./src/app.ts",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "dist"
-    },
-    devtool: "inline-source-map",
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 use: "ts-loader",
-                exclude: /node_modules/
+                include: [path.resolve(__dirname, "src")],
+                exclude: /node_modules/,
             }
         ]
     },
     resolve: {
-        extensions: [".ts", ".js"] 
+        extensions: [".ts", ".js"]
+    },
+    output: {
+        publicPath: "dist",
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
     }
 }
